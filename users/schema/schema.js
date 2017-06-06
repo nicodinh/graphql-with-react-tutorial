59cfcf77538c66b506c3962f2a181a242446c848
Nicolas Azari <nicodinh@azari.fr>
2017-06-06 12:15:56 +0200

    Writing a root query

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -13,3 +13,19 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt }
   }
 });
+
+
+// if you give an id then I return to you an UserType
+// resolve() returns data
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+
+      }
+    }
+  }
+});
