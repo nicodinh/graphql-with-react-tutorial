d5965aea956d34c30aa4037a209ede9869bfca2d
Nicolas Azari <nicodinh@azari.fr>
2017-06-06 17:23:04 +0200

    Async resolve functions

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
+const axios   = require('axios');
 const _       = require('lodash');
+
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -37,6 +39,20 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const RootQuery2 = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(response => response.data);
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery2
 });
